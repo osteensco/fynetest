0a162219626476b598240e4f7aef00f1d059a636
Scott Osteen <osteensco@gmail.com>
2024-09-22 22:26:28 -0500

    working modal

--- main.go
+++ main.go
@@ -2,32 +2,14 @@ package main
 
 import (
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Hello")
+	c := w.Canvas()
 
-	oldText := "Hello Fyne!"
-	newText := "Welcome :)"
-
-	label := widget.NewLabel(oldText)
-	popup := widget.ShowModalPopUp(container.NewCenter(
-		widget.NewLabel("I'm a popup!"),
-	))
-	w.SetContent(
-		container.NewVBox(
-			label,
-			widget.NewButton("Click Me", func() {
-				if label.Text == oldText {
-					label.SetText(newText)
-				} else {
-					label.SetText(oldText)
-				}
-			}),
-		))
+	home(w, c)
 
 	w.ShowAndRun()
 }
